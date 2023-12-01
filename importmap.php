9f16682b1436c16a756258705503dc1d1b8669e5
Philippe Le Van <philippe.levan@kibatic.com>
2023-12-01 17:51:42 +0100

    turbo drive

--- importmap.php
+++ importmap.php
@@ -23,5 +23,14 @@ return [
     ],
     '@popperjs/core' => [
         'version' => '2.11.8',
-    ]
+    ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@symfony/stimulus-bundle' => [
+        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
+    '@hotwired/turbo' => [
+        'version' => '7.3.0',
+    ],
 ];
